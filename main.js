a6ab3c92b37d288040d1ee99bb18cb86796532ea
samhatoum <sam@hatoum.net>
2014-05-02 00:16:56 -0700

    rtd-unit now parses the xml files and updates the meteor-test-runner reports

--- main.js
+++ main.js
@@ -1,32 +1,97 @@
-console.log('**** RESTARTED *****');
-//Npm.require('jasmine-node').executeSpecsInFolder({
-//    specFolders: ['/Users/sam/WebstormProjects/rtd/rtd-unit/example/tests/simple.js']
-//});
-
 var jasmineCli = process.env.PWD + '/packages/rtd-unit/.npm/package/node_modules/jasmine-node/lib/jasmine-node/cli.js',
     spawn = Npm.require('child_process').spawn,
-    args = [];
+    parseString = Npm.require('xml2js').parseString,
+    glob = Npm.require('glob'),
+    fs = Npm.require('fs'),
+    _ = Npm.require('lodash'),
+    args = [],
+    TEST_REPORTS_DIR = process.env.PWD + '/tests/.reports/rtd-unit';
+
+var hashCode = function (s) {
+        return s.split("").reduce(function (a, b) {
+            a = ((a << 5) - a) + b.charCodeAt(0);
+            return a & a
+        }, 0);
+    },
+    regurgitate = function (data) {
+        // TODO this should to to a test-runner visible console
+        // TODO wait for linefeeds before consoling out, so the dots make sense
+        console.log(data.toString());
+    };
+deleteFolderRecursive = function (path) {
+    var files = [];
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        files.forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
 
 args.push(jasmineCli);
-
 args.push('--coffee');
 args.push('--color');
-
+args.push('--match');
+args.push('.*-rtd-unit\.');
+args.push('--matchall');
 args.push('--junitreport');
 args.push('--output');
-args.push(process.env.PWD + '/tests/.rtd-unit-report');
-
+args.push(TEST_REPORTS_DIR);
 args.push(process.env.PWD + '/packages/rtd-unit/lib');
 args.push(process.env.PWD + '/tests');
-jasmineNode = spawn('/usr/local/bin/node', args);
 
-var regurgitate = function (data) {
-    // TODO this should to to a test-runner visible console
-    console.log(data.toString());
-};
+
 jasmineNode.stdout.on('data', regurgitate);
 jasmineNode.stderr.on('data', regurgitate);
 
-jasmineNode.on('close', function (code) {
-    // TODO collect junit test reports and load them into the collection
+var closeFunc = Meteor.bindEnvironment(function () {
+    var newResults = [],
+        xmlFiles = glob.sync('**/TEST-*.xml', { cwd: TEST_REPORTS_DIR });
+    _.each(xmlFiles, function (xmlFile, index) {
+        parseString(fs.readFileSync(TEST_REPORTS_DIR + '/' + xmlFile), function (err, result) {
+            _.each(result.testsuites.testsuite, function (testsuite) {
+                _.each(testsuite.testcase, function (testcase) {
+                    var result = ({
+                        name: testcase.$.name,
+                        framework: 'rtd-unit',
+                        result: testcase.failure ? 'fail' : 'pass',
+                        timestamp: testsuite.$.timestamp,
+                        time: testcase.$.time,
+                        ancestors: [testcase.$.classname]
+                    });
+                    if (testcase.failure) {
+                        _.each(testcase.failure, function (failure) {
+                            result.failureType = failure.$.type;
+                            result.failureMessage = failure.$.message;
+                            result.failureStackTrace = failure._;
+                        });
+                    }
+                    result.id = 'rtd-unit:' + hashCode(xmlFile + testcase.$.classname + testcase.$.name);
+                    newResults.push(result.id);
+                    Meteor.call('postResult', result);
+                });
+            });
+        });
+        if (index === xmlFiles.length - 1) {
+            Meteor.call('resetReports', {framework: 'rtd-unit', notIn: newResults});
+        }
+    });
 });
+
+var rerunTests = function () {
+    deleteFolderRecursive(TEST_REPORTS_DIR);
+    spawn('/usr/local/bin/node', args).on('close', closeFunc);
+};
+
+MeteorTestRunnerTestFiles.find({targetFramework: 'rtd-unit'}).observe({
+    added: rerunTests,
+    changed: rerunTests,
+    removed: rerunTests
+});
+
--- package.js
+++ package.js
@@ -4,9 +4,12 @@ Package.describe({
 
 Npm.depends({
     'glob': '3.2.9',
+    "lodash": "2.4.1",
+    'xml2js': '0.4.2',
     'jasmine-node': '1.14.3'
 });
 
 Package.on_use(function (api) {
+    api.use(['meteor-test-runner']);
     api.add_files('main.js', ['server']);
 });

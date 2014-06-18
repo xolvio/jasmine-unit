818e429a0ae9fce59bed0d8f8016bd69d8e04477
alanning <alanning@gmail.com>
2014-06-17 23:33:18 -0400

    extract stubbing code out into separate `package-stubber` smart package

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-;
-(function () {
+;(function () {
 
   "use strict";
 
@@ -114,7 +113,10 @@
     Meteor.call('resetLogs', {framework: 'jasmine-unit'});
     rimraf.sync(testReportsPath);
 
-    stubPackages()
+    PackageStubber.stubPackages({
+      dontStub: ['moment'],
+      outfile: path.join('tests', 'a1-package-stub.js')
+    })
 
     var jasmineNode = spawn(process.execPath, args);
     jasmineNode.stdout.on('data', regurgitate);
@@ -122,198 +124,5 @@
     jasmineNode.on('close', closeFunc);
   }
 
-  function getTestPackageNames (pwd, path, glob) {
-    var smartJsonFiles,
-        names = []
-        
-    smartJsonFiles = glob.sync(path.join("**","smart.json"), 
-                               {cwd: path.join(pwd, "packages")})
-
-    _.each(smartJsonFiles, function (filePath) {
-      var smartJson,
-          fullPath = path.join(pwd, "packages", filePath);
-
-      try {
-        smartJson = JSON.parse(fs.readFileSync(fullPath, 'utf8'))
-        if (smartJson && smartJson.testPackage) {
-          names.push(path.dirname(filePath))
-        }
-      } 
-      catch (ex) {
-        DEBUG && console.log(filePath, ex)
-      }
-    })
-
-    return names
-  }  // end getTestPackageNames 
-
-
-  function getPackageExports (pwd, path, glob, packagesToIgnore) {
-    var packageJsFiles,
-        packageExports = [],
-        exportsRE = /api\.export\s*\(\s*(['"])(.*?)\1/igm;
-        
-    packageJsFiles = glob.sync(path.join("**", "package.js"), 
-                               {cwd: path.join(pwd, "packages")})
-
-    _.each(packageJsFiles, function (filePath) {
-      var file,
-          found;
-
-      if (shouldIgnorePackage(packagesToIgnore, filePath)) {
-        DEBUG && console.log('ignoring', path.dirname(filePath))
-        return
-      }
-
-      try {
-        file = fs.readFileSync(path.join(pwd, "packages", filePath), 'utf8')
-        while(found = exportsRE.exec(file)) {
-          DEBUG && console.log('found export', filePath, found[2])
-          packageExports.push(found[2])
-        }
-      } catch (ex) {
-        DEBUG && console.log('error reading file', filePath, ex)
-      }
-    })
-
-    return packageExports
-  }  // end getPackageExports
-
-
-  function stubPackages () {
-    var stubs = {},
-        packageJsFiles,
-        packageExports = [],
-        packagesToIgnore,
-        smartJsonFiles,
-        ignoreString,
-        ignoreRegex,
-        out = "",
-        outfile;
-
-    outfile = path.join(pwd, 'tests', 'a1-package-stub.js')
-
-    // identify packages to ignore (ie. test packages)
-    packagesToIgnore = getTestPackageNames(pwd, path, glob)
-    packagesToIgnore.push('velocity')
-    
-    packageExports = getPackageExports(pwd, path, glob, packagesToIgnore)
-
-
-    // build stubs
-    _.each(packageExports, function (name) {
-      DEBUG && console.log('mocking', name)
-      // mock global[name] object and stick it on the stubs object as stubs[name]
-      makeStub(name, stubs)
-    })
-
-    // prep for file write
-    for (var name in stubs) {
-      out += name + " = " + stubs[name] + ";\n\n";
-    }
-
-    fs.writeFileSync(outfile, out)
-  }  // end stubPackages
-
-  function stubObject (target, dest) {
-    var emptyFn = function emptyFn () {}
-
-    for (var field in target) {
-      var type = typeof target[field]
-      switch (type) {
-        case "number":
-          dest[field] = target[field]
-          break;
-        case "string":
-          dest[field] = target[field]
-          break;
-        case "function":
-          dest[field] = "EMPTY_FUNCTION";
-          break;
-        case "object":
-          if (target[field] === null) {
-            dest[field] = null
-          } else if (target[field] instanceof Date) {
-            dest[field] = new Date(target[field])
-          } else {
-            dest[field] = {}
-            stubObject(target[field], dest[field])
-          }
-          break;
-      }
-    }
-  }
-
-  function shouldIgnorePackage (packagesToIgnore, packagePath) {
-    return _.some(packagesToIgnore, function (packageName) {
-      return packagePath.indexOf(packageName) == 0
-    })
-  }
-
-  /**
-   * Neither JSON.stringify() nor .toString() work for 
-   * functions so "stub" functions by replacing them with
-   * string constant EMPTY_FUNCTION and then converting to 
-   * empty function code in string form.
-   *
-   * Since stub is written to a javascript file, functions 
-   * will be properly stubbed as empty functions when the 
-   * stub file is loaded.
-   *
-   * @method convertEmptyFn
-   * @param {String} str String to convert
-   * @return {String} string with all EMPTY_FUNCTION strings
-   *                        converted to empty function 
-   *                        javascript function declarations
-   *                        in string form
-   */
-  function convertEmptyFn (str) {
-    return str.replace(/"EMPTY_FUNCTION"/g,"function emptyFn () {}")
-  }
-
-  function makeStub (name, dest) {
-    var self = this,
-        target = global[name],
-        tmpDest = {};
-
-    if (typeof target == 'function') {
-      // ex. moment().format('MMM dd, YYYY')
-      target = target();
-      stubObject(target, tmpDest)
-      dest[name] = "function () { return " + 
-                   convertEmptyFn(JSON.stringify(tmpDest, null, 2)) + "; }"
-    } else {
-      // ex. Roles.addUsersToRoles()
-      stubObject(target, tmpDest)
-      dest[name] = convertEmptyFn(JSON.stringify(tmpDest, null, 2))
-    }
-
-
-/*
-    var metadata = moduleMocker.getMetadata(target);
-    // console.log("\n\n\n############ Test string (moment):\n\n", moment().format('MMMM Do YYYY, h:mm:ss a'));
-    // console.log("\n\n\n############ moment.format:\n\n", global['moment']().format );
-    // console.log("\n\n\n############ Metadata:\n\n", metadata );
-    var mock = moduleMocker.generateFromMetadata(metadata);
-    console.log("\n\n\n############ Test string (mock):\n\n", mock.format('MMMM Do YYYY, h:mm:ss a'));
-    console.log("\n called with", mock.format.mock.calls)
-    //console.log("\n\n\n############ Mock:\n\n", mock );
-    //fs.writeFileSync(path.join(pwd, "test.log"), JSON.stringify(mock.format.mock))
-
-    var tmp = {};
-
-    for (var field in mock) {
-      if (field != 'toString') {
-        tmp[field] = mock[field]
-      }
-    }
-    console.log(tmp)
-    //fs.writeFileSync(path.join(pwd, "test.log"), JSON.stringify(tmp))
-*/
-
-  }  // end makeStub
-
 
 })();
-
-
